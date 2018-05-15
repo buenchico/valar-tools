2a36ef06fa2431dc19c9d68cc24f3dddd137dd03
EC2 Default User <ec2-user@ip-172-31-45-238.us-east-2.compute.internal>
2018-05-15 13:48:16 +0000

    heroku deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,15 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require turbolinks
 //= require_tree
 
-<script>
   $(function() {
   $("#accordion").accordion();
   });
-</script>
