6fda578057b8cd187f74c0791309ae86b2a397dd
EC2 Default User <ec2-user@ip-172-31-45-238.us-east-2.compute.internal>
2018-08-16 20:29:05 +0000

    Heroku precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
 //= require jquery_ujs
 //= require jquery-ui
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
