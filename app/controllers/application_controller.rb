fc1f8a2569b3edc8243e90f108c2e1a52c9bde06
Buenchico <hammer_ortiz@hotmail.com>
2018-09-17 18:45:55 +0000

    Tool list to hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-    $tools = {'routes': {'title': 'Calculadora de rutas', 'short_title': 'Ruta', 'version': '9.03', 'path': 'travel_calculator', 'icon': 'white giant-icon fas fa-map-signs'}
+    $tools = {'routes': {'title': 'Calculadora de rutas', 'short_title': 'Ruta', 'version': '9.03', 'path': 'travel_calculator', 'icon': 'fas fa-map-signs'},
+              'missions': {'title': 'Calculadora de misiones', 'short_title': 'Misiones', 'version': '9.01', 'path': 'missions', 'icon': 'fas fa-dice white'}
               }
     
     $static_pages = {'Acerca de': '/about', 'Contacto': '/contact' }
