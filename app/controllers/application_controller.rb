552ccc81428f28b34efbfb1ce4cea3ceccb435a1
Buenchico <hammer_ortiz@hotmail.com>
2019-03-29 18:35:19 +0000

    Add commented mission tool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
     $tools = {'routes': {'title': 'Calculadora de rutas', 'short_title': 'Ruta', 'version': '9.03', 'path': 'travel_calculator', 'icon': 'fas fa-map-signs'},
+            #  'missions': {'title': 'Calculadora de rutas', 'short_title': 'Misiones', 'version': '9.03', 'path': 'missions', 'icon': 'fas fa-map-signs'},
               }
     
     $static_pages = {'Acerca de': '/about', 'Contacto': '/contact' }
