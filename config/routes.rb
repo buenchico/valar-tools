db104adf50fea4196e88dfbbd9d8d5ec24afdd96
Buenchico <hammer_ortiz@hotmail.com>
2019-04-04 13:20:38 +0000

    Custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,24 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   
-  resources :users
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
   
-  resources :sessions, only: [:new, :create, :destroy]
+  # resources :users
+  # resources :sessions, only: [:new, :create, :destroy]
   
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  resources :locations
+  # resources :locations
 
   get  '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 
-  get 'missions/', to: 'missions#index'
-  post 'missions/calculate', to: 'missions#calculate'
-  get 'missions/recipe', to: 'missios#recipe'
+  # get 'missions/', to: 'missions#index'
+  # post 'missions/calculate', to: 'missions#calculate'
+  # get 'missions/recipe', to: 'missions#recipe'
 
   get 'travel_calculator', to: 'travel_calculator#index'
   post 'travel_calculator/calculate', to: 'travel_calculator#calculate'
