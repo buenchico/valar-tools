874ace778b551caa4578e39d40bf5dd279fb0250
Buenchico <hammer_ortiz@hotmail.com>
2019-09-17 21:16:18 +0000

    global variables set when starting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,18 @@ class ApplicationController < ActionController::Base
   before_action :set_variables
   add_flash_types :error, :success, :info, :danger
 
-  def set_variables    
-    $tools = {'routes': {'title': 'Calculadora de rutas', 'short_title': 'Ruta', 'version': '1.03', 'path': '/travel_calculator', 'icon': 'fas fa-map-signs'},
+  $tools = {'routes': {'title': 'Calculadora de rutas', 'short_title': 'Ruta', 'version': '1.03', 'path': '/travel_calculator', 'icon': 'fas fa-map-signs'},
             'players': {'title': 'Lista de jugadores', 'short_title': 'Jugadores', 'version': '1.01', 'path': '/users', 'icon': 'fas fa-user'},
             #  'missions': {'title': 'Calculadora de rutas', 'short_title': 'Misiones', 'version': '9.03', 'path': '/missions', 'icon': 'fas fa-map-signs'},
               'armies': {'title': 'Lista de ejércitos', 'short_title': 'Ejércitos', 'version': '1.01', 'path': '/armies', 'icon': 'fas fa-chess-knight'},
               }
     
-    $master_tools = {'houses': {'title': 'Lista de casas', 'short_title': 'Casas', 'version': '1.01', 'path': '/houses', 'icon': 'fas fa-flag'},
+  $master_tools = {'houses': {'title': 'Lista de casas', 'short_title': 'Casas', 'version': '1.01', 'path': '/houses', 'icon': 'fas fa-flag'},
               }
     
-    $static_pages = {'Acerca de': '/about', 'Contacto': '/contact' }
-    
+  $static_pages = {'Acerca de': '/about', 'Contacto': '/contact' }
+
+  def set_variables      
     $active_houses = ["Master"]
     ($active_houses << House.where(active: true).order(:name).pluck(:name)).flatten! # SELECT house.name_es FROM house WHERE active = true
     
