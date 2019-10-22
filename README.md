3ab08f59d78002d68ec0ec171c7a9b101bda6946
Buenchico <hammer_ortiz@hotmail.com>
2019-10-22 14:44:49 +0000

    Expand readme

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Valar-tools
 
-Webapp containint tools to help players at 
+Webapp containing tools to help players at 
     [valar.es](www.valar.es) by David Ortiz
 
 [Valar](www.valar.es) is a community that host various roleplaying 
     and strategy games, usually in the world of <strong>A Song of Ice and Fire</strong>.
+    
+This webapp contains the following tools:
+    * Travel calculator version 1.03
+    * Players version 1.00
+    * Armies version 1.00
+    * Houses version 1.00
--- config/initializers/variables.rb
+++ config/initializers/variables.rb
@@ -1,9 +1,9 @@
 # config/initializers/variables.rb
   
   $tools = {'routes': {'title': 'Calculadora de rutas', 'short_title': 'Ruta', 'version': '1.03', 'path': '/travel_calculator', 'icon': 'fas fa-map-signs'},
-    'players': {'title': 'Lista de jugadores', 'short_title': 'Jugadores', 'version': '1.01', 'path': '/users', 'icon': 'fas fa-user'},
+    'players': {'title': 'Lista de jugadores', 'short_title': 'Jugadores', 'version': '1.00', 'path': '/users', 'icon': 'fas fa-user'},
     # 'missions': {'title': 'Calculadora de rutas', 'short_title': 'Misiones', 'version': '9.03', 'path': '/missions', 'icon': 'fas fa-map-signs'},
-    'armies': {'title': 'Lista de ejércitos', 'short_title': 'Ejércitos', 'version': '1.01', 'path': '/armies', 'icon': 'fas fa-chess-knight'}
+    'armies': {'title': 'Lista de ejércitos', 'short_title': 'Ejércitos', 'version': '1.00', 'path': '/armies', 'icon': 'fas fa-chess-knight'}
     }
     
   $master_tools = {'houses': {'title': 'Lista de casas', 'short_title': 'Casas', 'version': '1.01', 'path': '/houses', 'icon': 'fas fa-flag'}
