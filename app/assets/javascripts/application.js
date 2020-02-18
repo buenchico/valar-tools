9819a4e217c3bd82f7d0f9e2be8ea0b37e0c2ab1
Buenchico <hammer_ortiz@hotmail.com>
2020-02-18 16:17:39 +0000

    require leaflet-easyprint changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 //= require jquery-tablesorter
 //= require leaflet
 //= require leaflet.draw
-//= require leaflet-easyprint/dist/bundle
+//= require leaflet-easyprint
 //= require rails-ujs
 //= require bootstrap
 //= require turbolinks
