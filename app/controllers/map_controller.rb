d19d3e8c112bcb3bc9330c539e8ec6222dda5328
Buenchico <hammer_ortiz@hotmail.com>
2020-10-05 20:06:59 +0100

    refactoring map.js so there is no static asset

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,13 @@
 class MapController < ApplicationController
   def index
+    @locations_main = Location.where('visibility != 99').where('location_type != ?','Ruinas').order(:visibility)
+    @location_secondary = Location.where(visibility: 99)
+    @location_ruins = Location.where(location_type: "Ruinas").where('kingdom != ?','El Muro')
+    @location_wall = Location.where(location_type: "Ruinas").where(kingdom: 'El Muro')
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 end
