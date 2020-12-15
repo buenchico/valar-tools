b32232d4cf3705dab1e558b6f3c1e33919eb9cb0
Buenchico <hammer_ortiz@hotmail.com>
2020-12-15 19:10:00 +0000

    hex map display js

--- app/controllers/hex_map_controller.rb
+++ app/controllers/hex_map_controller.rb
@@ -1,4 +1,18 @@
 class HexMapController < ApplicationController
   def index
+    @hex_map = {0=>{
+                  0=>{:name=>"Coruscant", :type=>"star-red", :info=>1},
+                  1=>{:name=>"Tattoine", :type=>"star-red", :info=>3}
+                },
+                1=>{
+                  2=> {:name=>"Sol", :type=>"star-red", :info=>2}
+                },
+                -3=>{
+                  3=> {:type=>"empty", :info=>2},
+                  2=> {:type=>"empty", :info=>2},
+                  1=> {:type=>"empty", :info=>2},
+                  0=> {:type=>"empty", :info=>2}
+                }
+              }
   end
 end
